f5b5a18f5749d32268fde143e8ab1b113db85bf0
Ahmed ElSaeed <114514662+asmpro7@users.noreply.github.com>
2023-02-24 12:12:44 +0200

    1A

--- TheatreSquare.py
+++ TheatreSquare.py
@@ -1,3 +1,8 @@
+#####################
+### author: asmpy ###
+### problem: 1A   ###
+#####################
+
 from math import ceil
 lenths=input().split()
 print(ceil(int(lenths[0])/int(lenths[-1]))*ceil(int(lenths[1])/int(lenths[-1])))
